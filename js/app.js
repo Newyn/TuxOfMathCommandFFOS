dd3df206292c9a58e19e6712c01efd35a857e74f
vtriquet <virgil.triquet@gmail.com>
2012-12-18 11:42:13 +0100

    Ajout des igloos

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var requestAnimationFrame =
 /* Création du canvas */
 var canvas = document.createElement("canvas");
 canvas.id = "game";
-canvas.width = 1000;
+canvas.width = 900;
 canvas.height = 600;
 
 /* Création du contexte */
@@ -22,18 +22,26 @@ var previousTimestamp = 0;
 var elapsedTime = 0;
 
 /* Tableau pour stocker les comètes */
-var arrayComete = [];
-
+var aListComete = [];
 /* Position des colonnes où apparaissent les comètes */
-var arrayColonneX = [150, 350, 550, 750];
+var aListColonneX = [150, 350, 550, 750];
 
-/* Fonction Step */
-var step = function () {
-	addComete();
+/* Tableau pour stocker les igloos */
+var aListIgloo = [];
+
+for (var i=0;i<4;i++) {
+
+	var oIglooImage = new Image();
+	oIglooImage.src = "resources/igloos/intact.png";
+	//oIglooImage.onload = function () {
+		var oIgloo = new Igloo(oIglooImage, aListColonneX[i] - 60, 500);
+		aListIgloo.push(oIgloo);
+	//}
 }
 
-/* Fonction addComete */
-var addComete = function() {
+	
+/* Fonction Step */
+var step = function () {
 
 	/* Réinitialisation de l'affichage */
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -41,18 +49,32 @@ var addComete = function() {
 	/* Remplissage du canvas */
     ctx.fillStyle = "rgb(0,0,0)";
     ctx.fillRect(0,0,canvas.width,canvas.height);
-		
+	
+	addIgloo();
+	addComete();
+}
+
+/* Fonction addIgloo */
+var addIgloo = function() {
+	
+	for(var i=0; i<aListIgloo.length; i++) {
+		ctx.drawImage(aListIgloo[i]._img, aListIgloo[i]._x, aListIgloo[i]._y);
+	}
+}
+/* Fonction addComete */
+var addComete = function() {
+
 	/* Mise à jour de l'intervalle d'apparition des comètes */
 	previousTimestamp = currentTimestamp;
 	currentTimestamp = Date.now();
 	elapsedTime += currentTimestamp - previousTimestamp;
 		
 	/* 3000 = 3 secondes */
-	if (elapsedTime >= 3000) {
+	if (elapsedTime >= 2000) {
 		
 		/* Création d'une nouvelle comète et ajout au tableau */
-		var oComete = new Comete(arrayColonneX[Math.floor(Math.random() * 4)], 50, "rgb(255,255,255)");
-		arrayComete.push(oComete);
+		var oComete = new Comete(aListColonneX[Math.floor(Math.random() * 4)], 50, "rgb(255,255,255)");
+		aListComete.push(oComete);
 		
 		ctx.beginPath();
 		ctx.fillStyle = oComete.rgb;
@@ -62,10 +84,10 @@ var addComete = function() {
 		elapsedTime = 0;
 	}
 	
-	for(var i=0;i<arrayComete.length;i++) {
+	for(var i=0;i<aListComete.length;i++) {
 		ctx.beginPath();
 		ctx.fillStyle = "rgb(255,255,255)";
-		ctx.arc(arrayComete[i].x, arrayComete[i].y++*0.5, arrayComete[i].radius, arrayComete[i].startAngle, arrayComete[i].endAngle);
+		ctx.arc(aListComete[i].x, aListComete[i].y++*0.5, aListComete[i].radius, aListComete[i].startAngle, aListComete[i].endAngle);
 		ctx.fill();
 	}
 	
---
+++ js/classes/Igloo.js
@@ -0,0 +1,5 @@
+function Igloo(img, x, y) {
+	this._img = img;
+	this._x = x;
+	this._y = y;
+}
